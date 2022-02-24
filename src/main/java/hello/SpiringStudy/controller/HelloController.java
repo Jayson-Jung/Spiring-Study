080de452f3a3efe870867ba294ce743320d8cf91
jayson-jung <hyeonsuns@kakao.com>
2022-02-25 00:45:44 +0900

    Api usage Study

--- src/main/java/hello/SpiringStudy/controller/HelloController.java
+++ src/main/java/hello/SpiringStudy/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,8 +16,34 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
-    public String hellomvc(@RequestParam("name") String name, Model model) {
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //Body부에 이 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;  //뷰가 없고 이 문자가 그대로 내려간다.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   //객체 리턴 (JSON 형태)
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {   //프로퍼티 접근방식, 자바빈 기본방식
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
