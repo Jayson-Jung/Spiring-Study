2b40805195fba14e2451402c47cbea3f110427e4
jayson-jung <hyeonsuns@kakao.com>
2022-02-24 19:59:48 +0900

    Initial commit

---
+++ src/main/java/hello/SpiringStudy/SpiringStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.SpiringStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/SpiringStudy/SpiringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.SpiringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
