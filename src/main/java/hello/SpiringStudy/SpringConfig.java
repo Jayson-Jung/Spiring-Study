dca281b0c527261ee67db85fc62eaa83af1306b2
jayson-jung <hyeonsuns@kakao.com>
2022-03-01 02:55:16 +0900

    AOP Practice

--- src/main/java/hello/SpiringStudy/SpringConfig.java
+++ src/main/java/hello/SpiringStudy/SpringConfig.java
@@ -25,14 +25,14 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository);
+        return new MemberService(memberRepository());
     }
 
-//  @Bean
-//  public MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
 //      return new MemoryMemberRepository();
 //      return new JdbcMemberRepository(dataSource);
-//      return new JdbcTemplateMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
 //      return new JpaMemberRepository(em);
-//  }
+    }
 }
--- src/main/java/hello/SpiringStudy/domain/Member.java
+++ src/main/java/hello/SpiringStudy/domain/Member.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
